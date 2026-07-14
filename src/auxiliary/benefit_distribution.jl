c5760325481e7385ae02abddb540c4baff0df655
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2026-07-15 07:42:43 +1000

    Removes RCP from benefit distribution analysis
    The last pd_main simulations have not used multiple RCPs. This commit
    removes RCP from the benefit distribution analysis.

--- src/auxiliary/benefit_distribution.jl
+++ src/auxiliary/benefit_distribution.jl
@@ -1,6 +1,6 @@
 using CSV, DataFrames, CairoMakie
 
-param = param_rcp[11][1]
+param = params[1]
 rcp="45"
 condition =
     rs.inputs.N_seed_CA .== param[1] * N_seed_weights.N_seed_CA .&&
@@ -11,18 +11,7 @@ condition =
 idx_scens = findall(condition)
 scenario_result = ADRIA.analysis.pathway_diversity(rs, idx_scens)
 
-param = param_rcp[11][1]
-rcp="70"
-condition =
-    rs.inputs.N_seed_CA .== param[1] * N_seed_weights.N_seed_CA .&&
-    rs.inputs.N_seed_TA .== param[1] * N_seed_weights.N_seed_TA .&&
-    rs.inputs.min_iv_locations .== param[2] .&&
-    rs.inputs.dhw_scenario .== param[3] .&&
-    rs.inputs.RCP .== parse(Float64, rcp)
-idx_scens = findall(condition)
-scenario_result = ADRIA.analysis.pathway_diversity(rs, idx_scens)
-
-param = param_rcp[29][1]
+param = params[4]
 rcp="45"
 condition =
     rs.inputs.N_seed_CA .== param[1] * N_seed_weights.N_seed_CA .&&
@@ -33,18 +22,16 @@ condition =
 idx_scens = findall(condition)
 scenario_result = ADRIA.analysis.pathway_diversity(rs, idx_scens)
 
-raw_rel_tac_low_seed_low_rcp = CSV.read("./Outputs/raw_rel_tac_seed_1e6_rcp45.csv", DataFrame; header=false, types=Float64)
-raw_rel_tac_low_seed_high_rcp = CSV.read("./Outputs/raw_rel_tac_seed_1e6_rcp70.csv", DataFrame; header=false, types=Float64)
-raw_rel_tac_high_seed_low_rcp = CSV.read("./Outputs/raw_rel_tac_seed_1e8_rcp45.csv", DataFrame; header=false, types=Float64)
+raw_rel_tac_low_seed = CSV.read("./Outputs/raw_rel_tac_seed_1e6.csv", DataFrame; header=false, types=Float64)
+raw_rel_tac_high_seed = CSV.read("./Outputs/raw_rel_tac_seed_1e8.csv", DataFrame; header=false, types=Float64)
 
-raw_fd_low_seed_low_rcp = CSV.read("./Outputs/raw_fd_seed_1e6_rcp45.csv", DataFrame; header=false, types=Float64)
-raw_fd_low_seed_high_rcp = CSV.read("./Outputs/raw_fd_seed_1e6_rcp70.csv", DataFrame; header=false, types=Float64)
-raw_fd_high_seed_low_rcp = CSV.read("./Outputs/raw_fd_seed_1e8_rcp45.csv", DataFrame; header=false, types=Float64)
+raw_fd_low_seed = CSV.read("./Outputs/raw_fd_seed_1e6.csv", DataFrame; header=false, types=Float64)
+raw_fd_high_seed = CSV.read("./Outputs/raw_fd_seed_1e8.csv", DataFrame; header=false, types=Float64)
 
-raw_rel_tac = (raw_rel_tac_low_seed_low_rcp, raw_rel_tac_low_seed_high_rcp, raw_rel_tac_high_seed_low_rcp)
-raw_fd = (raw_fd_low_seed_low_rcp, raw_fd_low_seed_high_rcp, raw_fd_high_seed_low_rcp)
+raw_rel_tac = (raw_rel_tac_low_seed, raw_rel_tac_low_seed_high_rcp, raw_rel_tac_high_seed)
+raw_fd = (raw_fd_low_seed, raw_fd_low_seed_high_rcp, raw_fd_high_seed)
 
-titles = ("seed=1e6, RCP=45", "seed=1e6, RCP=70", "seed=1e8, RCP=45")
+titles = ("seed=1e6, RCP=45", "seed=1e8, RCP=45")
 
 fig_rel_tac = Figure(; size=(1200, 400))
 for (i, data) in enumerate(raw_rel_tac)
@@ -79,7 +66,7 @@ save("./Outputs/benefit_function.png", fig)
 
 #=
 # Log `raw` to a file depending on which call this is (identified by σ).
-# Log to be placed inside two_sided_cvar
+# Log to be placed inside two_sided_cvar. REMEMBER TO REMOVE THREADS
 _raw_file = σ == _σ_rel_tac ? "./Outputs/raw_rel_tac.csv" : "./Outputs/raw_fd.csv"
 open(_raw_file, "a") do io
     println(io, "$(cvar_lower),$(cvar_upper),$(raw)")
